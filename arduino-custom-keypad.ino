17b2cb66f810f5aa89660275e17038ec7b9a4133
mesmaroth <mesmaroth@gmail.com>
2020-12-22 18:13:08 -0800

    Updated Arduino sketch

--- arduino-custom-keypad.ino
+++ arduino-custom-keypad.ino
@@ -1,6 +1,5 @@
-#include <HID-Project.h>
+ #include <HID-Project.h>
 
-const int pin_led = 2;
 const int push_button_1 = 3;
 const int push_button_2 = 4;
 const int push_button_3 = 5;
@@ -8,60 +7,76 @@ const int push_button_4 = 6;
 const int push_button_5 = 7;
 const int push_button_6 = 8;
 
+const int delay_button_press = 300;
+
 void setup() {
-  pinMode(pin_led, OUTPUT);
   pinMode(push_button_1, INPUT_PULLUP);
   pinMode(push_button_2, INPUT_PULLUP);
   pinMode(push_button_3, INPUT_PULLUP);
   pinMode(push_button_4, INPUT_PULLUP);
   pinMode(push_button_5, INPUT_PULLUP);
   pinMode(push_button_6, INPUT_PULLUP);
-   
-  Consumer.begin();
 }
 
 
 void loop() {
   if (!digitalRead(push_button_1)) {
-    digitalWrite(pin_led, HIGH);
+    Consumer.begin();
+    // digitalWrite(pin_led, HIGH);
     Consumer.write(MEDIA_PREVIOUS);
-    delay(200); // wait
-    digitalWrite(pin_led, LOW);
+    delay(delay_button_press);
+    // digitalWrite(pin_led, LOW);
+    Consumer.end();
   }
   
   if (!digitalRead(push_button_2)) {
-    digitalWrite(pin_led, HIGH);
+    Consumer.begin();
+    // digitalWrite(pin_led, HIGH);
     Consumer.write(MEDIA_PLAY_PAUSE);
-    delay(200); // wait
-    digitalWrite(pin_led, LOW);
+    delay(delay_button_press);
+    // digitalWrite(pin_led, LOW);
+    Consumer.end();
   }
     
   if (!digitalRead(push_button_3)) {
-    digitalWrite(pin_led, HIGH);
+    Consumer.begin();
+    // digitalWrite(pin_led, HIGH);
     Consumer.write(MEDIA_NEXT);
-    delay(200); // wait
-    digitalWrite(pin_led, LOW);
+    delay(delay_button_press);
+    // digitalWrite(pin_led, LOW);
+    Consumer.end();
   }
     
   if (!digitalRead(push_button_4)) {
-    digitalWrite(pin_led, HIGH);
-    Consumer.write(MEDIA_PLAY_PAUSE);
-    delay(200); // wait
-    digitalWrite(pin_led, LOW);
+    Keyboard.begin();
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F5);
+    delay(delay_button_press);
+    Keyboard.releaseAll();
+    Keyboard.end();
   }
     
   if (!digitalRead(push_button_5)) {
-    digitalWrite(pin_led, HIGH);
-    Consumer.write(MEDIA_PLAY_PAUSE);
-    delay(200); // wait
-    digitalWrite(pin_led, LOW);
+    Keyboard.begin();
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F6);
+    delay(delay_button_press);
+    Keyboard.releaseAll();
+    Keyboard.end();
   }
     
   if (!digitalRead(push_button_6)) {
-    digitalWrite(pin_led, HIGH);
-    Consumer.write(MEDIA_PLAY_PAUSE);
-    delay(200); // wait
-    digitalWrite(pin_led, LOW);
+    Keyboard.begin();
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press(KEY_LEFT_ALT);
+    Keyboard.press(KEY_LEFT_SHIFT);
+    Keyboard.press(KEY_F7);
+    delay(delay_button_press);
+    Keyboard.releaseAll();
+    Keyboard.end();
   }
-  
 }
